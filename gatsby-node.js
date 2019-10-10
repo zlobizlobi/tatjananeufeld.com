ccc040278346e52c9099a78ea81f281945d2fe28
David <bureauzlobi@gmail.com>
2019-10-10 21:10:47 +0200

    Add styled-components to the project

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,15 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require("path")
+exports.onCreateWebpackConfig = ({ actions }) => {
+    actions.setWebpackConfig({
+        resolve: {
+            alias: {
+                components: path.resolve(__dirname, "src/components"),
+                sections: path.resolve(_dirname, "src/components")
+            },
+        },
+    })
+}
