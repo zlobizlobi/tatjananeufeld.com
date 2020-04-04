6855482dca73170bd202a98f522d21132e485636
David Zlobinskyy <bureauzlobi@gmail.com>
2020-04-04 03:32:40 +0200

    Remove redundant code

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,5 @@
 const path = require('path');
 
-// require('dotenv').config({
-//     path: `.env.${process.env.NODE_ENV}`,
-// });
-
 module.exports = {
     siteMetadata: {
         siteUrl: 'https://www.tatjananeufeld.com',
