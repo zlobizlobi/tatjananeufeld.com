1d3e8e647e0e38723af6cd313532ab3169a4767a
David Zlobinskyy <bureauzlobi@gmail.com>
2020-07-24 00:26:21 +0200

    feature: add gatsby-ssr onWrapPageElement

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Layout from './src/components/layout';
 
-const LayoutRenderer = ({ PageComponent, pageProps }) =>
-    <Layout {...pageProps}>
-        <PageComponent {...pageProps} />
+export const wrapPageElement = ({ element, props }) =>
+    <Layout {...props}>
+        {element}
     </Layout>
 
-export const replaceComponentRenderer = ({ props: { pageResources, ...pageProps } }) =>
-    <LayoutRenderer PageComponent={pageResources.component} pageProps={pageProps} />
+export const wrapRootElement = ({ element }) => (
+    <>
+        <GlobalStyle />
+        {element}
+    </>
+)
