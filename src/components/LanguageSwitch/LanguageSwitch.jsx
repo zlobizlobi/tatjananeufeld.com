b15f5d32d324724774171300fd846a43484c0b9f
David Zlobinskyy <bureauzlobi@gmail.com>
2020-10-06 11:39:21 +0100

    fix: language not switching correctly in LanguageSwitch in build

--- src/components/LanguageSwitch/LanguageSwitch.jsx
+++ src/components/LanguageSwitch/LanguageSwitch.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Container } from './styles';
 import { changeLocale, useIntl } from 'gatsby-plugin-intl';
 
 export const LanguageSwitch = ({ className }) => {
+    const [pageLanguage, setPageLanguage] = useState(null);
+
     const { locale } = useIntl();
+
+    useEffect(() => {
+        setPageLanguage(locale);
+    }, [locale]);
 
     return (
         <Container className={className}>
             {['en', 'de', 'ru'].map(language => (
                 <Button
                     key={language}
-                    isActive={language === locale}
+                    isActive={pageLanguage === language}
                     onClick={() => changeLocale(language)}
                 >
                     {language}
